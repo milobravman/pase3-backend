39041cd23f24991365b482a30e5ba4a135262833
Milo_Lynch <Milobravman20@gmail.com>
2020-10-20 15:33:59 -0400

    most recent commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -72,14 +72,77 @@ parts = [
         monster_id: monsterObj_ary[1].id
     },
     {
+        name: "metal-body",
+        part_type: "chest",
+        attack_power: 20,
+        health: 175,
+        image: "https://mcishop.azureedge.net/mciassets/w_9_0043150_edward-steel-armour-set_550.png",
+        monster_id: monsterObj_ary[1].id
+    },
+    {
+        name: "silouet-legs",
+        part_type: "legs",
+        attack_power: 20,
+        health: 175,
+        image: "https://i.pinimg.com/originals/79/dc/50/79dc50484e5f07d8de7afa374a52fd54.jpg",
+        monster_id: monsterObj_ary[1].id
+    },
+    {
+        name: "shield-arm",
+        part_type: "Larm",
+        attack_power: 20,
+        health: 175,
+        image: "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/75126/shield-clipart-md.png",
+        monster_id: monsterObj_ary[1].id
+    },
+    {
         name: "goblin-head",
+        part_type: "Rarm",
+        attack_power: 20,
+        health: 175,
+        image: "https://previews.123rf.com/images/lineartestpilot/lineartestpilot1404/lineartestpilot140402256/27829907-cartoon-monster-arm.jpg",
+        monster_id: monsterObj_ary[1].id
+    },
+    {
+        name: "cookie-head",
         part_type: "head",
         attack_power: 20,
         health: 175,
         image: "https://pixy.org/src/420/thumbs350/4209771.jpg",
         monster_id: monsterObj_ary[2].id
+    },
+    {
+        name: "diomond-chest",
+        part_type: "chest",
+        attack_power: 0,
+        health: 2075,
+        image: "https://www.pngitem.com/pimgs/m/126-1261802_minecraft-diamond-chestplate-minecraft-diamond-armor-png-transparent.png",
+        monster_id: monsterObj_ary[2].id
+    },
+    {
+        name: "rocket-boosters",
+        part_type: "legs",
+        attack_power: 300,
+        health: 75,
+        image: "https://webstockreview.net/images/clipart-rocket-thrust-1.png",
+        monster_id: monsterObj_ary[2].id
+    },
+    {
+        name: "snake-arm",
+        part_type: "Larm",
+        attack_power: 300,
+        health: 75,
+        image: "https://webstockreview.net/images/clipart-snake-3.jpg",
+        monster_id: monsterObj_ary[2].id
+    },
+    {
+        name: "corn-arm",
+        part_type: "Rarm",
+        attack_power: 3,
+        health: 15,
+        image: "https://www.kindpng.com/picc/m/349-3497489_milho-ear-of-corn-ears-of-corn-food.png",
+        monster_id: monsterObj_ary[2].id
     }
-    
 ]
 
 
